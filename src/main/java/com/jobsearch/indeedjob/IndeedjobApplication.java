4e5ab0ee216c39a402a6e3cabee2fab13872bcec
abiaki997-tech <abiakhil69@gmail.com>
2025-03-06 21:42:23 +0530

    Initial commit

---
+++ src/main/java/com/jobsearch/indeedjob/IndeedjobApplication.java
@@ -0,0 +1,13 @@
+package com.jobsearch.indeedjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndeedjobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndeedjobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobsearch/indeedjob/IndeedjobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobsearch.indeedjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndeedjobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
